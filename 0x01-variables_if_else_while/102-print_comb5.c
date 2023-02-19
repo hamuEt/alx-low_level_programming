109366faaf21aa55b72e8a7049291d6826684133
hamid <hamidzeyne50@gmail.com>
2023-02-19 06:40:29 -0800

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,19 @@ int main(void)
 {
 int d, p;
 
-for (d = '0'; d < '9'; d++)
+for (d = '0'; d <= '98'; d++)
 {
-for (p = d + 1; p <= '9'; p++)
+for (p = d + 1; p <= '98'; p++)
 {
-if (p != d)
-{
-putchar(d);
-putchar(p);
-if (d == '8' && p == '9')
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+putchar(' ');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+if (d == '98' && p == '99')
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
