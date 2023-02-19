c2a153fff35f471d100007842a21ea3aa62c550c
hamid <hamidzeyne50@gmail.com>
2023-02-19 01:23:44 -0800

    hellow world last tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 *main - Entry point
 *Return: Always 0 (Success)
@@ -6,7 +7,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
